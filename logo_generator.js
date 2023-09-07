d9cd153ee5688bc9e7238b473d4eb9aeaf3aad98
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-09-07 14:40:51 -0500

    Function for Logo

--- logo_generator.js
+++ logo_generator.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
 const { createSVG } = require('svg.js');
 
+function createLogo(text, textColor, shape, shapeColor) {
+    const canvas = createSVG(300, 200);
+  
+    let shapeElement;
+    switch (shape) {
+      case 'circle':
+        shapeElement = canvas.circle(150, 100, 50).fill(shapeColor);
+        break;
+      case 'triangle':
+        shapeElement = canvas.polygon([[150, 50], [100, 150], [200, 150]]).fill(shapeColor);
+        break;
+      case 'square':
+        shapeElement = canvas.rect(100, 50, 100, 100).fill(shapeColor);
+        break;
+      default:
+        console.error('Invalid shape');
+        process.exit(1);
+    }
+}

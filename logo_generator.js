b2abb784400f2d6a08b39cfada79116433d0de33
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-09-07 18:42:09 -0500

    test

--- logo_generator.js
+++ logo_generator.js
@@ -1,63 +1,70 @@
-const inquirer = require("inquirer");
 const fs = require('fs');
-const { createSVG } = require('svg.js');
+const inquirer = require('inquirer');
 
-function createLogo(text, textColor, shape, shapeColor) {
-    const canvas = createSVG(300, 200);
-  
-    let shapeElement;
-    switch (shape) {
-      case 'circle':
-        shapeElement = canvas.circle(150, 100, 50).fill(shapeColor);
-        break;
-      case 'triangle':
-        shapeElement = canvas.polygon([[150, 50], [100, 150], [200, 150]]).fill(shapeColor);
-        break;
-      case 'square':
-        shapeElement = canvas.rect(100, 50, 100, 100).fill(shapeColor);
-        break;
-      default:
-        console.error('Invalid shape');
-        process.exit(1);
-    }
-        // Add text to the canvas
-        canvas.text(text).move(10, 50).fill(textColor);
-    
-        // Export the SVG to a file named 'logo.svg'
-        fs.writeFileSync('logo.svg', canvas.svg());
+// Function to generate the SVG file
+function generateSVG(text, textColor, shape, shapeColor) {
+  let svgContent;
+
+  if (shape === 'circle') {
+    svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="50" fill="${shapeColor}" />
+        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
+      </svg>
+    `;
+  } else if (shape === 'triangle') {
+    svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="150,50 100,150 200,150" fill="${shapeColor}" />
+        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
+      </svg>
+    `;
+  } else if (shape === 'square') {
+    svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="${shapeColor}" />
+        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
+      </svg>
+    `;
+  }
+
+  fs.writeFileSync('logo.svg', svgContent);
+  console.log('Generated logo.svg');
 }
 
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'userText',
-            message: 'Enter the text:',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter text color (e.g., "red", "#00ff00"):',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape:',
-            choices: ['circle', 'triangle', 'square'],
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter shape color (e.g., "blue", "#ff9900"):',
-        },
-    ])
-    .then((answers) => {
-      const { userText, textColor, shape, shapeColor } = answers;
-
-      createLogo(userText, textColor, shape, shapeColor);
-
-      console.log('Generated logo.svg');
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => {
+        if (input.length <= 3) {
+          return true;
+        }
+        return 'Please enter up to three characters.';
+      },
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (color keyword or hexadecimal number):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (color keyword or hexadecimal number):',
+    },
+  ])
+  .then((answers) => {
+    generateSVG(answers.text, answers.textColor, answers.shape, answers.shapeColor);
   })
   .catch((error) => {
-      console.error(error);
+    console.error(error);
   });

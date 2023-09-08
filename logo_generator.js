35e53acd0129de3f21e948f276361f7366a89e4e
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-09-07 22:22:44 -0500

    Added A Lib/Shapes

--- logo_generator.js
+++ logo_generator.js
@@ -1,35 +1,42 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const {Circle} = require('./lib/shapes');
+const {Triangle} = require('./lib/shapes');
+const {Square} = require('./lib/shapes');
 
 function generateSVG(text, textColor, shape, shapeColor) {
   let svgContent;
+  let shapeInstance;
 
   if (shape === 'circle') {
-    svgContent = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="150" cy="100" r="50" fill="${shapeColor}" />
-        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
-      </svg>
-    `;
+    shapeInstance = new Circle();
   } else if (shape === 'triangle') {
-    svgContent = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="150,50 100,150 200,150" fill="${shapeColor}" />
-        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
-      </svg>
-    `;
+    shapeInstance = new Triangle();
   } else if (shape === 'square') {
-    svgContent = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill="${shapeColor}" />
-        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
-      </svg>
-    `;
+    shapeInstance = new Square();
   }
+
+  // Debug: Print shape and shapeInstance
+  console.log('Selected Shape:', shape);
+  console.log('Shape Instance:', shapeInstance);
+
+  // Debug: Check if setColor method exists
+  if (typeof shapeInstance.setColor === 'function') {
+    shapeInstance.setColor(shapeColor);
+  } else {
+    console.error('setColor method not found on shapeInstance');
+  }
+
+  svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shapeInstance.render()}
+      <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
+    </svg>`;
 
   fs.writeFileSync('logo.svg', svgContent);
   console.log('Generated logo.svg');
 }
+
 
 inquirer
   .prompt([
